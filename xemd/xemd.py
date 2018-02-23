74f6c3d133e481a8a7f515b0d366d46df5824fc4
Richard Berry <rjsberry@protonmail.com>
2018-02-23 23:51:12 +0000

    Python argument assertions prior to calling C++ code

--- xemd/xemd.py
+++ xemd/xemd.py
@@ -25,16 +25,21 @@ def _validate_input_signal(function):
     """
     @wraps(function)
     def input_function_wrapper(*args, **kwargs):
+        if len(args) and args[0].ndim != 1:
+            raise ValueError("Input signal must have shape (N,)")
         function(*args, **kwargs)
     return input_function_wrapper
 
 
-def _validate_stopping_criterion(function):
+def _validate_s_number(function):
     """Ensures the stopping criterion specified for an xemd family
     function meets the required specification.
     """
     @wraps(function)
     def input_function_wrapper(*args, **kwargs):
+        if (kwargs.get('s_number') is not None and
+            kwargs.get('s_number') < 0):
+            raise ValueError("S number must be a positive integer")
         function(*args, **kwargs)
     return input_function_wrapper
 
@@ -45,24 +50,30 @@ def _validate_auxillary_parameters(function):
     """
     @wraps(function)
     def input_function_wrapper(*args, **kwargs):
+        if (kwargs.get('ensemble_size') is not None and
+            kwargs.get('ensemble_size') < 0):
+            raise ValueError("Ensemble size must be a positive integer")
+        if (kwargs.get('noise_strength') is not None and
+           kwargs.get('noise_strength') < 0):
+            raise ValueError("Ensemble size ")
         function(*args, **kwargs)
     return input_function_wrapper
 
 
 @_validate_input_signal
-@_validate_stopping_criterion
+@_validate_s_number
 def emd(signal,
-        stopping_criterion=None):
+        s_number=None):
     """ Main C++ empirical mode decomposition entry point.
     """
     return xemd_core.emd(signal)
 
 
 @_validate_input_signal
-@_validate_stopping_criterion
+@_validate_s_number
 @_validate_auxillary_parameters
 def eemd(signal,
-         stopping_criterion=None,
+         s_number=None,
          ensemble_size=None,
          noise_strength=None):
     """ Main C++ ensemble empirical mode decomposition entry point.
@@ -71,10 +82,10 @@ def eemd(signal,
 
 
 @_validate_input_signal
-@_validate_stopping_criterion
+@_validate_s_number
 @_validate_auxillary_parameters
 def ceemdan(signal,
-            stopping_criterion=None,
+            s_number=None,
             ensemble_size=None,
             noise_strength=None):
     """ Main C++ complete ensemble empirical mode decomposition with
