17c648568e811de9dc9f18ba9ad8dfe668b16a88
Richard Berry <rjsberry@protonmail.com>
2018-02-24 00:31:17 +0000

    Python frotend API tests: input signal dimensions

---
+++ test/test_xemd_entry_point_assertions.py
@@ -0,0 +1,73 @@
+# xemd, Copyright (c) 2018, Richard Berry <rjsberry@protonmail.com>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+
+from datetime import datetime
+
+import numpy as np
+import pytest
+
+import xemd
+
+RNG_SEED = datetime.now().second
+
+
+@pytest.fixture
+def assert_called():
+    class AssertCall(object):
+        def __init__(self):
+            self.called = False
+        def __call__(self, *args, **kwargs):
+            self.called = True
+    return AssertCall()
+
+
+# Numpy signal dimensions
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.emd", assert_called)
+    xemd.emd(np.random.rand(np.random.randint(RNG_SEED + 1)))
+    assert assert_called.called
+
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.eemd", assert_called)
+    xemd.eemd(np.random.rand(np.random.randint(RNG_SEED + 1)))
+    assert assert_called.called
+
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.ceemdan", assert_called)
+    xemd.ceemdan(np.random.rand(np.random.randint(RNG_SEED + 1)))
+    assert assert_called.called
+
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.emd", assert_called)
+    with pytest.raises(ValueError):
+        xemd.eemd(np.random.rand(np.random.randint(RNG_SEED + 1), 2))
+    assert not assert_called.called
+
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.eemd", assert_called)
+    with pytest.raises(ValueError):
+       xemd.eemd(np.random.rand(np.random.randint(RNG_SEED + 1), 2))
+    assert not assert_called.called
+
+
+def test_emd_np_array_1dim_assertions(monkeypatch, assert_called):
+    monkeypatch.setattr("xemd_core.ceemdan", assert_called)
+    with pytest.raises(ValueError):
+        xemd.ceemdan(np.random.rand(np.random.randint(RNG_SEED + 1), 2))
+    assert not assert_called.called
