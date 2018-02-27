179dfdae8b20aa7194adb39b362b91f73eef0f36
Richard Berry <rjsberry@protonmail.com>
2018-02-27 07:10:09 +0000

    Update source file license headers

--- test/test.hpp
+++ test/test.hpp
@@ -1,19 +1,14 @@
-// xemd, Copyright (c) 2018, Richard Berry <rjsberry@protonmail.com>
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 
-// * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// 
-// * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
-
 #ifndef INCLUDE_XEMD_UNIT_TEST_INIT_HPP_
 #define INCLUDE_XEMD_UNIT_TEST_INTI_HPP_
+
+// xemd: https://github.com/rjsberry/xemd
+//
+// Copyright (C) 2018, Richard Berry <rjsberry@protonmail.com>
+//
+// Distributed under the terms of BSD 2-Clause "simplified" license. (See
+// accompanying file LICENSE, or copy at
+// https://github.com/rjsberry/xemd/blob/master/LICENSE)
+//
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
--- test/test_xemd_xutils.cpp
+++ test/test_xemd_xutils.cpp
@@ -1,16 +1,11 @@
-// xemd, Copyright (c) 2018, Richard Berry <rjsberry@protonmail.com>
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 
-// * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// 
-// * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
+// xemd: https://github.com/rjsberry/xemd
+//
+// Copyright (C) 2018, Richard Berry <rjsberry@protonmail.com>
+//
+// Distributed under the terms of BSD 2-Clause "simplified" license. (See
+// accompanying file LICENSE, or copy at
+// https://github.com/rjsberry/xemd/blob/master/LICENSE)
+//
 
 #include <xtensor/xrandom.hpp>
 #include <xtensor/xtensor.hpp>
